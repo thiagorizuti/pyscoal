e0855a155fb233a22af4c64bff17719a70fed49d
Thiago <t.rizuti@gmail.com>
2021-05-18 18:52:52 -0300

    bic fitness function and ranked selection

--- src/pyscoal/__init__.py
+++ src/pyscoal/__init__.py
@@ -1,8 +1,9 @@
 __version__ = '0.1.0'
 
-__all__ = ['SCOAL', 'MSCOAL','make_dataset']
+__all__ = ['SCOAL', 'MSCOAL','EvoSCOAL','make_dataset']
 
 from .algorithms._scoal import SCOAL
 from .algorithms._mscoal import MSCOAL
+from .algorithms._evoscoal import EvoSCOAL
 from .data._make_dataset import make_dataset
 
